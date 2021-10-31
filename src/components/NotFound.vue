f62f5687c4fbe4648eeb40a5b82b8aa39e27690c
tersawy <mohamedtersawy96@gmail.com>
2021-10-31 18:26:42 +0200

    Added NotFound component

---
+++ src/components/NotFound.vue
@@ -0,0 +1,8 @@
+<template>
+	<v-container>
+		<v-layout row wrap align-center justify-center column class="mt-16 mb-16 pt-8" style="height:20rem">
+			<h1 style="font-size: 6rem">4<span class="red--text" style="font-size: 8rem">o</span>4</h1>
+			<h1>Not Found</h1>
+		</v-layout>
+	</v-container>
+</template>
